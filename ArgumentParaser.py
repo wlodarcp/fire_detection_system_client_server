d1c4178a9052325529468ae3080ce107f546cde2
wlodarcp <Pan.Wlodarczyk.Pawel@gmail.com>
2020-11-13 22:06:04 +0100

    Added argument paraser and extracted video source provider

---
+++ ArgumentParaser.py
@@ -0,0 +1,9 @@
+import argparse
+
+def parase_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-H', default='localhost', type=str, help="Server HOST to connect")
+    parser.add_argument('-P', default=8098, type=int, help="Server port to connect")
+    parser.add_argument('--source_video', type=str, help="Path for video source - if not set video from camera on local machine is streamed")
+    parser.add_argument('--fire_detection', type=bool, default=False, help="Is fire detection enabled - by default false")
+    return parser.parse_args()
--- client.py
+++ client.py
@@ -1,27 +1,57 @@
 # -*- coding: utf-8 -*-
-import cv2
-import numpy as np
-import socket
 import pickle
+import socket
 import struct
 
-cap=cv2.VideoCapture(0)
-clientsocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-clientsocket.connect(('localhost',8098))
+import cv2
 
-while True:
-    ret,frame=cap.read()
-    # Serialize frame
-    data = pickle.dumps(frame)
-    # Send message length first
-    message_size = struct.pack("L", len(data))
-    is_fire = struct.pack("?", True)
-    # Then data
-    clientsocket.sendall(is_fire + message_size + data)
-    
-    if cv2.waitKey(1) & 0xFF ==ord('q'):
-        break
-    
-cap.release()  
-clientsocket.stop()
-    
+from ArgumentParaser import parase_arguments
+from VideoSourceProvider import get_video_capture
+
+
+def connect_to_server_socket(HOST, port):
+    clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    clientsocket.connect((HOST, port))
+    return clientsocket
+
+
+def stream_video_with_fire_detection_signal(cap, clientsocket):
+    while True:
+        ret, frame = cap.read()
+        # Serialize frame
+        data = pickle.dumps(frame)
+        # Send message length first
+        message_size = struct.pack("L", len(data))
+        # First byte is boolean with detection result - for now for testing always True
+        is_fire = struct.pack("?", True)
+        # Then data
+        clientsocket.sendall(is_fire + message_size + data)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+
+def stream_video_without_fire_detection_signal(cap, clientsocket):
+    while True:
+        ret, frame = cap.read()
+        # Serialize frame
+        data = pickle.dumps(frame)
+        # Send message length first
+        message_size = struct.pack("L", len(data))
+        # Then data
+        clientsocket.sendall(message_size + data)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+
+if __name__ == "__main__":
+    args = parase_arguments()
+    clientsocket = connect_to_server_socket(args.H, args.P)
+    cap = get_video_capture(args.source_video)
+    if args.fire_detection:
+        stream_video_with_fire_detection_signal(cap, clientsocket)
+    else:
+        stream_video_without_fire_detection_signal(cap, clientsocket)
+    cap.release()
+    clientsocket.stop()
