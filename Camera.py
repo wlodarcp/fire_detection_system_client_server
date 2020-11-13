063a26cc9f4e20d4a8daed6d838c1fec85d4ca37
wlodarcp <Pan.Wlodarczyk.Pawel@gmail.com>
2020-11-13 20:55:45 +0100

    Added is_fire_detected_in_long_measurement - method that uses signal from last 20 frames (stored in queue) and if more that half signals were positive returns true - what means that there is fire detected on video from camera

--- Camera.py
+++ Camera.py
@@ -1,12 +1,23 @@
+from collections import deque
+
+
 class Camera:
-    cameras = []
 
     def __init__(self, id, port, has_fire_detection_enabled):
         self.id = id
         self.port = port
         self.has_fire_detection_enabled = has_fire_detection_enabled
-        self.cameras.append(id)
+        if self.has_fire_detection_enabled:
+            self.fire_signal_queue = deque([False] * 20, maxlen=20)
 
     def __getitem__(self, key):
         return key
 
+    def update_fire_signal_queue(self, fire_signal):
+        if self.has_fire_detection_enabled:
+            self.fire_signal_queue.appendleft(fire_signal)
+
+    def is_fire_detected_in_long_measurement(self):
+        if self.has_fire_detection_enabled:
+            return self.fire_signal_queue.count(True)/20 > 0.5
+        return False
