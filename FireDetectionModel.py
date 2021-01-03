dd95a0d7d6fb2f7b5172d5f1a8a79f9e4e8f89d0
wlodarcp <Pan.Wlodarczyk.Pawel@gmail.com>
2021-01-03 21:31:12 +0100

    Minor refactor - added whitespaces to improve readability

--- FireDetectionModel.py
+++ FireDetectionModel.py
@@ -17,22 +17,27 @@ def chose_input_shape():
 
 def load_model(model_name):
     input_shape = chose_input_shape()
+
     model = Sequential()
     model.add(Conv2D(32, (3, 3), input_shape=input_shape))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
+
     model.add(Conv2D(32, (3, 3)))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
+
     model.add(Conv2D(64, (3, 3)))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
+
     model.add(Flatten())
     model.add(Dense(64))
     model.add(Activation('relu'))
     model.add(Dropout(0.5))
     model.add(Dense(1))
     model.add(Activation('sigmoid'))
+
     model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
     model.load_weights(model_name)
     return model
