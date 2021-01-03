abb4a2fdc5ab1e09f135111a22d269b8f6d7ada4
wlodarcp <Pan.Wlodarczyk.Pawel@gmail.com>
2021-01-03 21:27:00 +0100

    Extracted QUEUE_SIZE and FIRE_TIME_RATE to variables, removed unused __getitem__ method

--- Camera.py
+++ Camera.py
@@ -3,15 +3,15 @@ from collections import deque
 
 class Camera:
 
+    QUEUE_SIZE = 20
+    FIRE_TIME_RATE = 0.9
+
     def __init__(self, id, port, has_fire_detection_enabled):
         self.id = id
         self.port = port
         self.has_fire_detection_enabled = has_fire_detection_enabled
         if self.has_fire_detection_enabled:
-            self.fire_signal_queue = deque([False] * 20, maxlen=20)
-
-    def __getitem__(self, key):
-        return key
+            self.fire_signal_queue = deque([False] * self.QUEUE_SIZE, maxlen=self.QUEUE_SIZE)
 
     def update_fire_signal_queue(self, fire_signal):
         if self.has_fire_detection_enabled:
@@ -19,5 +19,5 @@ class Camera:
 
     def is_fire_detected_in_long_measurement(self):
         if self.has_fire_detection_enabled:
-            return self.fire_signal_queue.count(True)/20 > 0.9
+            return self.fire_signal_queue.count(True)/self.QUEUE_SIZE > self.FIRE_TIME_RATE
         return False
