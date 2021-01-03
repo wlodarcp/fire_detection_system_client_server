787cf380335a1bc245f9440f221e42088c628eb9
wlodarcp <Pan.Wlodarczyk.Pawel@gmail.com>
2021-01-03 21:27:33 +0100

    Fixed configuration parsing

--- Configuration.py
+++ Configuration.py
@@ -8,7 +8,6 @@ from Camera import Camera
 class Configuration:
     cameras: list
     is_video_saving_enabled: bool
-    is_fire_detection_signal_check_enabled: bool
     HOST: str
 
 
@@ -16,7 +15,7 @@ def load_configuration(config_file_name):
     with open(config_file_name) as f:
         config = yaml.safe_load(f)
         loaded_cameras = load_cameras_from_config(config['cameras'])
-        configuration = Configuration(loaded_cameras, config['is_video_saving_enabled'], config['is_fire_detection_signal_check_enabled'], config['HOST'])
+        configuration = Configuration(loaded_cameras, config['is_video_saving_enabled'], config['HOST'])
         print(configuration)
         return configuration
 
